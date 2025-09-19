880e473542e3f36c909164b18251d30ff7d2babd
sal-gitt <ucs160@outlook.com>
2025-09-19 08:56:30 +0530

    Update ex6afinal.py

--- ex6afinal.py
+++ ex6afinal.py
@@ -1,44 +1,28 @@
-res = []
-buffer = []
-buffer_size = 0
-operators = {"(": 0, "/" : 1, "*" : 1, "+" : 2, "-" : 2} 
-exp = input("Enter infix expression: ")
-for char in exp:
-    if char.isalnum():
-        res.append(char)
-    else:
-        if len(buffer) == 0:
-            buffer.append(char)
-            buffer_size += 1
-            continue
-        temp1 = operators.get(buffer[-1])
-        if operators.get(char) < temp1:
-            if char == "(":
-                buffer.append(char)
-                buffer_size += 1
-                marker1 = buffer_size
-                continue
-            buffer.append(char)
-            buffer_size += 1
-        elif char == ")":
-            res.extend(buffer[-1:marker1])
-            buffer_size -= len(buffer[-1:marker1]) + 1
-            del buffer[-1:marker1+1]
-        else:
-            temp2 = operators.get(char) 
-            for char in reversed(buffer):
-                if operators.get(char) > temp2:
-                    marker2 = buffer.index(char)
-                    res.extend(buffer[-1:marker2])
-                    buffer_size -= len(buffer[-1:marker2]) + 1
-                    del buffer[-1:marker2+1]
-                    break
-            buffer.append(char)
-            buffer_size += 1
-while len(buffer) != 0:
-    for char in reversed(buffer):
-        res.append(char)
-        buffer.remove(char)
-buffer.clear()
+def infix_to_postfix(expression):
+    precedence = {"(": 0, "+" : 1, "-" : 1, "*" : 2, "/" : 2}
+    output = []
+    stack = []
 
-print("".join(res))
+    for char in expression:
+        if char.isalnum():  # Operand
+            output.append(char)
+        elif char == "(":   # Left parenthesis
+            stack.append(char)
+        elif char == ")":   # Right parenthesis
+            while stack and stack[-1] != "(":
+                output.append(stack.pop())
+            stack.pop()  # Remove '(' from stack
+        else:  # Operator
+            while stack and precedence[char] <= precedence[stack[-1]]:
+                output.append(stack.pop())
+            stack.append(char)
+
+    # Pop remaining operators from the stack
+    while stack:
+        output.append(stack.pop())
+
+    return "".join(output)
+
+exp = input("Enter infix expression: ")
+postfix = infix_to_postfix(exp)
+print("Postfix expression:", postfix)

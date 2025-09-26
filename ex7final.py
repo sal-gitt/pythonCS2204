87dc479f25a3f0cd789c834a56ea983a801c7837
sal-gitt <ucs160@outlook.com>
2025-09-26 10:45:42 +0530

    Update ex7final.py

--- ex7final.py
+++ ex7final.py
@@ -2,31 +2,33 @@ class BinaryTree:
     def __init__(self):
         self.tree = []
 
-    def visualize_preorder(self, i):
-        print(self.tree[i], end = ", ")
+    def visualize_preorder(self, i = 0):
+        print(self.tree[i], end = " ")
         if (2*i+1) < len(self.tree):
-            visualize_preorder(2*i+1)
+            self.visualize_preorder(2*i+1)
         if (2*i+2) < len(self.tree):
-            visualize_preorder(2*i+2)
+            self.visualize_preorder(2*i+2)
         
-    def visualize_inorder(self, i):
+    def visualize_inorder(self, i = 0):
         if (2*i+1) < len(self.tree):
-            visualize_inorder(2*i+1)
-        print(self.tree[i], end = ", ")
+            self.visualize_inorder(2*i+1)
+        print(self.tree[i], end = " ")
         if (2*i+2) < len(self.tree):
-            visualize_inorder(2*i+2)
+            self.visualize_inorder(2*i+2)
             
-    def visualize_postorder(self, i):
+    def visualize_postorder(self, i = 0):
         if (2*i+1) < len(self.tree):
-            visualize_postorder(2*i+1)
+            self.visualize_postorder(2*i+1)
         if (2*i+2) < len(self.tree):
-            visualize_postorder(2*i+2)
-        print(self.tree[i], end = ", ")
+            self.visualize_postorder(2*i+2)
+        print(self.tree[i], end = " ")
             
 bt = BinaryTree()
 nodes = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 bt.tree.extend(nodes)
 
 bt.visualize_preorder()
+print()
 bt.visualize_inorder()
+print()
 bt.visualize_postorder()

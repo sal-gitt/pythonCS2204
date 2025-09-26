6fd36c2d151942117c13b77741161f7eb0462135
sal-gitt <ucs160@outlook.com>
2025-09-26 10:35:27 +0530

    Update ex7final.py

--- ex7final.py
+++ ex7final.py
@@ -4,23 +4,23 @@ class BinaryTree:
 
     def visualize_preorder(self, i):
         print(self.tree[i], end = ", ")
-        if (2i+1) < len(self.tree):
-            visualize_preorder(2i+1)
-        if (2i+2) < len(self.tree):
-            visualize_preorder(2i+2)
+        if (2*i+1) < len(self.tree):
+            visualize_preorder(2*i+1)
+        if (2*i+2) < len(self.tree):
+            visualize_preorder(2*i+2)
         
     def visualize_inorder(self, i):
-        if (2i+1) < len(self.tree):
-            visualize_inorder(2i+1)
+        if (2*i+1) < len(self.tree):
+            visualize_inorder(2*i+1)
         print(self.tree[i], end = ", ")
-        if (2i+2) < len(self.tree):
-            visualize_inorder(2i+2)
+        if (2*i+2) < len(self.tree):
+            visualize_inorder(2*i+2)
             
     def visualize_postorder(self, i):
-        if (2i+1) < len(self.tree):
-            visualize_postorder(2i+1)
-        if (2i+2) < len(self.tree):
-            visualize_postorder(2i+2)
+        if (2*i+1) < len(self.tree):
+            visualize_postorder(2*i+1)
+        if (2*i+2) < len(self.tree):
+            visualize_postorder(2*i+2)
         print(self.tree[i], end = ", ")
             
 bt = BinaryTree()
